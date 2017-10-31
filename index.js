c064492727f4285d441c266d35cd38a076968289
Kevin Randles <krandles@gmail.com>
2017-10-31 12:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
  cart[item] = [Math.floor((Math.random() * 99) + 1)]
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
